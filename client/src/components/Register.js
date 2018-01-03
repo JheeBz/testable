67bf6d1541094037fc58da7307785f24a162604f
JheeBz <christian_kirkeby@hotmail.com>
2018-01-03 07:24:16 +0000

    Added Formik, Yup, and implemented Register component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Button,
   Card,
   CardBody,
@@ -7,95 +8,156 @@ import {
   Col,
   Form,
   FormGroup,
+  FormFeedback,
   FormText,
   Input
 } from 'reactstrap'
-import React, { Component } from 'react'
+import React from 'react'
+import { withFormik } from 'formik'
+import Yup from 'yup'
+import AuthenticationService from '../services/AuthenticationService'
 
-export default class Register extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: '',
-      confirmPassword: ''
+/**
+ * @todo add successful message to top of form
+ */
+
+const RegisterForm = ({ 
+  values,
+  errors,
+  status,
+  touched,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting
+}) => (
+  <Card className="Register">
+    <CardBody>
+      <CardTitle className="text-center">Register</CardTitle>
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col xs={10} className="mx-auto">
+            {status && status.error ? (
+              <Alert color="danger">{status.error}</Alert>
+            ) : (
+              status && status.message && <Alert>{status.message}</Alert>
+            )}
+            <FormGroup>
+              {/* @todo refactor className conditional logic. Potentially a function. */}
+              <Input
+                autoFocus
+                type="email"
+                name="email"
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="email"
+                className={touched.email && ((status && status.email) || errors.email) ? 'is-invalid' : null}
+                placeholder="Email"
+              />
+              {status && status.email ? (
+                <FormFeedback>{status.email}</FormFeedback>
+              ) : (
+                errors.email && touched.email && <FormFeedback>{errors.email}</FormFeedback>
+              )}
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="password"
+                className={touched.password && ((status && status.password) || errors.password) ? 'is-invalid' : null}
+                placeholder="Password"
+              />
+              {status && status.password ? (
+                <FormFeedback>{status.password}</FormFeedback>
+              ) : (
+                errors.password && touched.password && <FormFeedback>{errors.password}</FormFeedback>
+              )}
+              <FormText muted>
+                Must be at least 8 characters long, contain 1 number and 1
+                UPPERCASE letter.
+              </FormText>
+            </FormGroup>
+            <Button
+              color="success"
+              className="d-block mx-auto"
+              type="submit"
+              disabled={isSubmitting}>
+              Register
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </CardBody>
+  </Card>
+)
+
+const Register = withFormik({
+  mapPropsToValues({ email, password }) { 
+    return {
+      email: '', 
+      password: ''
+    }
+  },
+  /**
+   * @todo Grab access token from response once implemented.
+   * @todo Make password requirements identical to API requirements.
+   */
+  handleSubmit(values, { setSubmitting, setStatus, setErrors }) {
+    setSubmitting(true)
+    register({
+      email: values.email,
+      password: values.password
+    }, { setStatus, setErrors })
+    setSubmitting(false)
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup
+      .string()
+      .email('Email address is not valid.')
+      .required('Email address is required.'),
+    password: Yup
+      .string()
+      .min(8, 'Password must be at least 8 characters.')
+      .required('Password is required.')
+  })
+})(RegisterForm)
+
+const register = async (credentials, { setStatus, setErrors }) => {
+  try {
+    const res = await AuthenticationService.register({
+      email: credentials.email,
+      password: credentials.password
+    })
+    if (res.status === 201) {
+      setStatus({
+        message: 'Successfully registered! Redirecting...'
+      })
+    }
+  } catch (error) {
+    const errKeys = Object.keys(error.response.data.error)
+
+    switch (errKeys[0]) {
+      case 'email':
+        setErrors({
+          email: error.response.data.error[errKeys[0]]
+        })
+        break
+      case 'password':
+        setErrors({
+          password: error.response.data.error[errKeys[0]]
+        })
+        break
+      default:
+        setStatus({
+          error: error.response.data.error
+        })
     }
   }
-
-  handleSubmit = event => {
-    event.preventDefault()
-  }
-
-  handleInputChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    })
-  }
-
-  validateForm() {
-    return (
-      this.state.email.length > 0 &&
-      this.state.password.length > 0 &&
-      this.state.confirmPassword.length > 0
-    )
-  }
-
-  render() {
-    return (
-      <Card className="Register">
-        <CardBody>
-          <CardTitle className="text-center">Register</CardTitle>
-          <Form onSubmit={this.handleSubmit}>
-            <Row>
-              <Col xs={10} className='mx-auto'>
-                <FormGroup>
-                  <Input
-                    autoFocus
-                    type="email"
-                    name="email"
-                    value={this.state.username}
-                    onChange={this.handleInputChange}
-                    id="email"
-                    placeholder="Email"
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Input
-                    type="password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.handleInputChange}
-                    id="password"
-                    placeholder="Password"
-                  />
-                  <FormText muted>
-                    Must be at least 8 characters long, contain 1 number and 1
-                    UPPERCASE letter.
-                  </FormText>
-                </FormGroup>
-                <FormGroup>
-                  <Input
-                    type="password"
-                    name="confirmPassword"
-                    value={this.state.confirmPassword}
-                    onChange={this.handleInputChange}
-                    id="confirmPassword"
-                    placeholder="Confirm Password"
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-          </Form>
-          <Button
-            color="success"
-            className="d-block mx-auto"
-            type="submit"
-            disabled={!this.validateForm()}
-          >
-            Register
-          </Button>
-        </CardBody>
-      </Card>
-    )
-  }
 }
+
+export default Register
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-export default () => {
-  return axios.create({
-    baseURL: 'localhost:8081'
+export default () =>  
+  axios.create({
+    baseURL: 'http://localhost:8082/'
   })
-}
